0ae534f36bf1c77fa713c268eb4751950efda077
Data Structures and Algorithms in C and Assembly Language <mcintosh1798@gmail.com>
2025-08-30 20:32:27 -0400

    Add files via upload

--- util/util.h
+++ util/util.h
@@ -17,7 +17,7 @@
     51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
 ------------------------------------------------------------------------------*/
 #ifndef UTIL_H
-#define UTIL_H
+#define UTIL_H  1
 
 #include <stddef.h>
 #include <stdlib.h>
